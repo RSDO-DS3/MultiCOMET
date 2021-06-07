3b8e770bf2cc0fcc47bd4d413fbc378f1a5150bd
AdrianMG <52002338+AMGrobelnik@users.noreply.github.com>
2021-06-07 21:43:38 +0200

    Model Organization

--- Setup/DownloadContent.sh
+++ Setup/DownloadContent.sh
@@ -12,5 +12,6 @@ rm download.zip
 GetUnzipFile https://www.dropbox.com/sh/5pty18i8k07ymuu/AADbCv9uypqfA4sqsNV5cmMOa?dl=0 models
 GetUnzipFile https://www.dropbox.com/sh/d9f11jiyk042959/AABhH6wP9tPe_xrZnNVj--ZVa?dl=0 pretrained_models
 GetUnzipFile https://www.dropbox.com/sh/slz3hf91fl11nww/AACArhUlKtAKOFQ09syFzx7_a?dl=0 MULTI_COMET_DATA
+GetUnzipFile https://www.dropbox.com/sh/44uk9pwoun4i09c/AAC0xqSTKPZ2psWP4vLlwOaja?dl=0 MULTI_COMET_MODELS
 GetUnzipFile https://www.dropbox.com/sh/o0rvxjo12nj2vr4/AABqSGsiYeGB38ITteRrflzfa?dl=0 data
 GetUnzipFile https://www.dropbox.com/sh/64kn98wiap9cgau/AAAGDI84XTXaaelhZRzXr7o_a?dl=0 model
